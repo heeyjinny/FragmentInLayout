e332ad293164176b0e91c5167050f34505e7a9b2
heeyjinny <heeyjinny@gmail.com>
2022-11-14 07:50:24 +0900

    화면전환 없는 프래그먼트 한 개 화면 표시

--- app/src/main/java/com/heeyjinny/fragmentinlayout/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/fragmentinlayout/MainActivity.kt
@@ -2,10 +2,23 @@ package com.heeyjinny.fragmentinlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.heeyjinny.fragmentinlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //뷰바인딩
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        //뷰바인딩
+        setContentView(binding.root)
+
+        //프래그먼트 생성
+        //java디렉터리 밑 패키지명 우클릭 - New - Fragment - Fragment(Blank)
+        //ListFragment.kt 생성
+
+    }//onCreate
+
+}//MainActivity
